6be29b0ca07adb1ab472bc4ef3c0a87bb25faff6
Joseph Holtkamp <76000251+holtkampjs@users.noreply.github.com>
2025-02-28 18:17:29 +0000

    almost done with high card tests

---
+++ java/src/main/java/com/sourceallies/interview/service/PokerService.java
@@ -0,0 +1,73 @@
+package com.sourceallies.interview.service;
+
+import java.util.HashMap;
+
+import io.micrometer.common.util.StringUtils;
+
+public class PokerService {
+
+    HashMap<String, Integer> rankingMap = new HashMap<>();
+    HashMap<Integer, String> cardNameMap = new HashMap<>();
+    private char[] handRankings = {'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
+
+    public PokerService() {
+        rankingMap.put("2", 2);
+        rankingMap.put("3", 3);
+        rankingMap.put("4", 4);
+        rankingMap.put("5", 5);
+        rankingMap.put("6",6);
+        rankingMap.put("7", 7);
+        rankingMap.put("8", 8);
+        rankingMap.put("9", 9);
+        rankingMap.put("T", 10);
+        rankingMap.put("J", 11);
+        rankingMap.put("Q", 12);
+        rankingMap.put("K", 13);
+        rankingMap.put("A", 14);
+
+        cardNameMap.put(10, "TEN");
+        cardNameMap.put(11, "JACK");
+        cardNameMap.put(12, "QUEEN");
+        cardNameMap.put(13, "KING");
+        cardNameMap.put(14, "ACE");
+    }
+
+    public String pokerHand(String blackHand, String whiteHand) {
+        var blackHandValues = blackHand.split(" ");
+        var whiteHandValues = whiteHand.split(" ");
+
+        String highHand = "";
+        int whiteHighCard = 0;
+        int blackHighCard = 0;
+
+        for (int i = 0; i < blackHandValues.length ; i++) {
+            int rank = rankingMap.get(blackHandValues[i]);
+            if (blackHighCard < rank) {
+                blackHighCard = rank;
+            }
+        }
+
+        for (int i = 0; i < whiteHandValues.length ; i++) {
+            int rank = rankingMap.get(whiteHandValues[i]);
+            if (whiteHighCard < rank) {
+                whiteHighCard = rank;
+            }
+        }
+
+        if (whiteHighCard > blackHighCard) {
+            String cardValue = cardNameMap.get(whiteHighCard);
+            if (cardValue != null) {
+                return "White hand wins - high card: " + cardValue;
+            } else {
+                return "White hand wins - high card: " + whiteHighCard;
+            }
+        } else {
+            String cardValue = cardNameMap.get(blackHighCard);
+            if (cardValue != null) {
+                return "Black hand wins - high card: " + cardValue;
+            } else {
+                return "Black hand wins - high card: " + blackHighCard;
+            }
+        }
+    }
+}
--- java/src/test/java/com/sourceallies/interview/SolutionTest.java
+++ java/src/test/java/com/sourceallies/interview/SolutionTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import com.sourceallies.interview.service.PokerService;
+
 public class SolutionTest {
     
     @Test
@@ -12,5 +14,44 @@ public class SolutionTest {
         assertEquals("Hello, world!", solution.getGreeting());
     }
 
+    @Test
+    void testHighCardScoreAceWins() {
+        PokerService pokerService = new PokerService();
+        String result = pokerService.pokerHand("2 3 5 6 7", "A J 4 9 3");
     
+        assertEquals("White hand wins - high card: ACE", result);
+    }
+
+
+    @Test
+    void testHighCardScoreJack() {
+        PokerService pokerService = new PokerService();
+        String result = pokerService.pokerHand("2 3 5 6 7", "2 J 4 9 3");
+    
+        assertEquals("White hand wins - high card: JACK", result);
+    }
+
+    @Test
+    void testHighCardNotTenOrAbove() {
+        PokerService pokerService = new PokerService();
+        String result = pokerService.pokerHand("2 3 5 6 7", "2 6 4 9 3");
+    
+        assertEquals("White hand wins - high card: 9", result);
+    }
+
+    @Test
+    void testHighCardBlackHandWins() {
+        PokerService pokerService = new PokerService();
+        String result = pokerService.pokerHand("Q K 4 8 3", "J 3 9 T J");
+    
+        assertEquals("Black hand wins - high card: KING", result);
+    }
+
+    @Test
+    void testHighCardTie() {
+        PokerService pokerService = new PokerService();
+        String result = pokerService.pokerHand("Q K 4 8 3", "J 3 9 T J");
+    
+        assertEquals("Black hand wins - high card: KING", result);
+    }
 }
